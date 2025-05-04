e504a12548924885edc7a7e1faffaee0527ef3c6
Dima Neto <kuznetsovdv2013@gmail.com>
2025-05-04 13:37:50 +0300

    добавил код в app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,18 @@ import sum from './basic';
 console.log('worked');
 
 console.log(sum([1, 2]));
+
+class ErrorRepository {
+    constructor() {
+        this.errors = new Map([
+            [400, 'Bad Request'],
+            [404, 'Not Found'],
+            [502, 'Bad Gateway'],
+            [504, 'Gateway Timeout']
+        ]);
+    }
+
+    translate(code) {
+        return this.errors.get(code) || 'Unknown error';
+    }
+}
